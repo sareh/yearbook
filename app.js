aff3246ac4001e196df7fa5d359596d143c63fb2
sareh <sareh.heidari@gmail.com>
2015-11-13 12:42:12 +0000

    App

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+//var ejs = require('ejs');
+
+var app = express();
+var expressHelpers = require('express-helpers')(app);
+var routes = require('./config/routes');
+
+// Connection to database
+var databaseUrl = 'mongodb://localhost/27017/yearbook'
+mongoose.connect(databaseUrl);
+
+// Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === "object" && "_method" in req.body){
+    var method = req.body._method;
+    delete req.body._method;
+    return method; 
+  }
+}));
+
+//app.use(routes);
+app.use(expressLayouts);
+app.use(express.static(__dirname + "/public"));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Listen
+app.listen(3000);
+console.log("Express is listening on localhost:3000");
