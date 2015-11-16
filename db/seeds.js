c62ccd168d864c121409573969e7dd4ebf988034
sareh <sareh.heidari@gmail.com>
2015-11-16 09:35:43 +0000

    Seeds for tag, project & person models, all inter-referenced

--- db/seeds.js
+++ db/seeds.js
@@ -2,38 +2,74 @@ var mongoose = require('mongoose');
 var databaseUrl = 'mongodb://localhost:27017/yearbook';
 mongoose.connect(databaseUrl);
 
-var Person = require('../models/person');
+var Person  = require('../models/person');
 var Project = require('../models/project');
+var Tag     = require('../models/tag');
 
-var projects = [
-  {
+function saveItemToDb(err, item){
+  if(err) console.log(err);
+  console.log("Item saved." + item);
+}
+
+var html5       = new Tag({name: "HTML5"});
+var css3        = new Tag({name: "CSS3"});
+var js          = new Tag({name: "JavaScript"});
+var ruby        = new Tag({name: "Ruby"});
+var rails       = new Tag({name: "Ruby on Rails"});
+var sinatra     = new Tag({name: "Sinatra"});
+var node        = new Tag({name: "NodeJS"});
+var angular     = new Tag({name: "AngularJS"});
+var react       = new Tag({name: "ReactJS"});
+var bootstrap   = new Tag({name: "Bootstrap"});
+var foundation  = new Tag({name: "Foundation"});
+var skeleton    = new Tag({name: "Skeleton"});
+var materialize = new Tag({name: "Materialize"});
+
+var tags = [html5, css3, js, ruby, rails, sinatra, node, angular, react, bootstrap, foundation, skeleton, materialize];
+
+tags.forEach(function(tag, index){
+  tag.save(saveItemToDb);
+});
+
+var pasur = new Project({
     title:      "Pasur",
     github:     "https://github.com/sareh/pasur",
     heroku:     "http://pasur.herokuapp.com/",
-    screenshot: ""
-  },
-  {
+    screenshot: "",
+    tags:       [html5, css3, js]
+  });
+
+var portal = new Project({
     title:      "Portal",
     github:     "https://github.com/sareh/portal",
     heroku:     "http://uni-portal.herokuapp.com/",
-    screenshot: ""
-  },
-  {
+    screenshot: "",
+    tags:       [html5, css3, ruby, rails, foundation]
+  });
+
+var lemonade = new Project({
     title:      "Lemonade Stand",
     github:     "https://github.com/martyjg/lemonade-stand",
     heroku:     "",
-    screenshot: "https://raw.githubusercontent.com/martyjg/lemonade-stand/master/Screen-Shot-Lemonade-2.jpg"
-  },
-  {
+    screenshot: "https://raw.githubusercontent.com/martyjg/lemonade-stand/master/Screen-Shot-Lemonade-2.jpg",
+    tags:       [html5, css3, js]    
+  });
+
+var screencap = new Project({
     title:      "Screencap",
     github:     "https://github.com/martyjg/screencap_app",
     heroku:     "",
-    screenshot: ""
-  }
-]
+    screenshot: "",
+    tags:       [html5, css3, ruby, rails, foundation]
+  });
 
-var people = [
-  {
+var projects = [pasur, portal, lemonade, screencap];
+
+projects.forEach(function(project, index){
+  project.save(saveItemToDb);
+});
+
+var sareh = {
     first_name: "Sareh",
     last_name: "Heidari",
     nickname: "",
@@ -42,9 +78,10 @@ var people = [
     linkedin: "https://www.linkedin.com/in/sarehheidari",
     twitter: "https://www.twitter.com/sareh88",
     type: "student",
-    projects: projects.sareh
-  },
-  {
+    projects: [pasur, portal]
+  };
+
+var marty = {
     first_name: "Marty",
     last_name: "Gormley",
     nickname: "",
@@ -53,21 +90,11 @@ var people = [
     linkedin: "",
     twitter: "https://www.twitter.com/martygormley",
     type: "student",
-    projects: projects.marty
-  }
-]
+    projects: [lemonade, screencap]
+  };
 
-function saveItemToDb(err, item){
-  if(err) console.log(err);
-  console.log("Item saved." + item);
-}
- 
-projects.forEach(function(project, index){
-  var newProject = new Project(project);
-  newProject.save(saveItemToDb);
-});
+var people = [sareh, marty];
 
 people.forEach(function(person, index){
-  var newPerson = new Person(person);
-  newPerson.save(saveItemToDb);
+  person.save(saveItemToDb);
 });
