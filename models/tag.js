305b72a8ea0e0663e178ac4fea7bfcff74ce58a8
sareh <sareh.heidari@gmail.com>
2015-11-16 09:54:29 +0000

    Make tag name unique

--- models/tag.js
+++ models/tag.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var tagSchema = new mongoose.Schema({
-  name: String,
+  name: {type: String, unique: true, required: true},
   projects:  [{
               type: mongoose.Schema.ObjectId,
               ref: 'Project'
