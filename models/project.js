2d43e65a88fca0fadae621068a1059ddd0610935
sareh <sareh.heidari@gmail.com>
2015-11-16 09:36:45 +0000

    Project model - linked with references to Tag model

--- models/project.js
+++ models/project.js
@@ -4,7 +4,15 @@ var projectSchema = new mongoose.Schema({
   title:      String,
   github:     String,
   heroku:     String,
-  screenshot: String
+  screenshot: String,
+  // creators:  [{
+  //             type: mongoose.Schema.ObjectId,
+  //             ref: 'Person'
+  //             }],
+  tags:      [{
+              type: mongoose.Schema.ObjectId,
+              ref: 'Tag'
+              }]
 });
 
 module.exports = mongoose.model('Project', projectSchema);
